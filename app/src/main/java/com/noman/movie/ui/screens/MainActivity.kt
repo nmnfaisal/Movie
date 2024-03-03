3b2eb37705034823d6106cefe5821614405f7ba1
Noman Faisal <nmnfaisal@gmail.com>
2024-03-03 23:29:32 +0500

    Created Folder Structure

--- app/src/main/java/com/noman/movie/ui/screens/MainActivity.kt
+++ app/src/main/java/com/noman/movie/ui/screens/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.noman.movie
+package com.noman.movie.ui.screens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.noman.movie.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
