73a399a87db543480a629ab1924fa2372fc0dd88
Noman Faisal <nmnfaisal@gmail.com>
2024-03-03 23:30:57 +0500

    Added hilt annotation to Application class and MainActivity

--- app/src/main/java/com/noman/movie/ui/screens/MainActivity.kt
+++ app/src/main/java/com/noman/movie/ui/screens/MainActivity.kt
@@ -3,7 +3,9 @@ package com.noman.movie.ui.screens
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.noman.movie.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
